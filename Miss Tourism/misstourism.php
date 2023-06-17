45a6b3f6c3b18b263fefc1b3ca17b9f6639b9ef1
dipoljoseph <97227464+dipoljoseph@users.noreply.github.com>
2023-06-17 17:30:27 +0800

    Add files via upload

---
+++ Miss Tourism/misstourism.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <title>Miss Tourism</title>
+</head>
+<body>
+    <!--header-->
+    <div class="header">
+        <div class="column">
+
+            <div class="left-col">
+                <img src="miss tourism heritage\logo_dark.png" class="h-logo">
+            </div>
+            
+            <div class="middle-col">
+                <img src="miss tourism heritage\001-envelope.png" class="h-icon">
+                <div class="h-row">
+                    <pre>SEND US AN EMAIL</pre>
+                    <pre><b>info@tourismheritage.com</b></pre>
+                </div>
+
+                <img src="miss tourism heritage\002-telephone-symbol-button.png" class="h-icon">
+                <div class="h-row">
+                    <pre>CALL US FOR MORE INFORMATIONS</pre>
+                    <pre><b>(+632) 1234-5678 ext. 0611</b></pre>
+                </div>
+            </div>
+
+            <div class="right-col">
+                <button class="cont-button">CONTACT US!</button>
+            </div>
+        </div>
+    </div>
+
+    <!--navigation/header-->
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <ul> 
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Our Directors</a></li>
+            <li><a href="#">Candidates</a></li>
+            <li><a href="#">News & Events</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </nav>
+    
+    <!--banner-->
+    <img src="miss tourism heritage\banner image.png" class="banner">
+
+    <!--feature candidates-->
+    <div class="ft-cand">
+        <h1>FEATURED CANDIDATES</h1>
+        <p>Lorem Ipsum dolor</p>
+
+        <div class="column">
+            <div class="card">
+                <img src="miss tourism heritage\candidate image.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>SAMPLE NAME HERE</b></h4>
+                    <p>City of Manila</p>
+                    <button class="view-det">View Details</button>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="miss tourism heritage\candidate image.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>SAMPLE NAME HERE</b></h4>
+                    <p>City of Manila</p>
+                    <button class="view-det">View Details</button>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="miss tourism heritage\candidate image.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>SAMPLE NAME HERE</b></h4>
+                    <p>City of Manila</p>
+                    <button class="view-det">View Details</button>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="miss tourism heritage\candidate image.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>SAMPLE NAME HERE</b></h4>
+                    <p>City of Manila</p>
+                    <button class="view-det">View Details</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="miss tourism heritage\candidate image.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>SAMPLE NAME HERE</b></h4>
+                    <p>City of Manila</p>
+                    <button class="view-det">View Details</button>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="miss tourism heritage\candidate image.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>SAMPLE NAME HERE</b></h4>
+                    <p>City of Manila</p>
+                    <button class="view-det">View Details</button>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="miss tourism heritage\candidate image.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>SAMPLE NAME HERE</b></h4>
+                    <p>City of Manila</p>
+                    <button class="view-det">View Details</button>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="miss tourism heritage\candidate image.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>SAMPLE NAME HERE</b></h4>
+                    <p>City of Manila</p>
+                    <button class="view-det">View Details</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--news and events-->
+    <div class="news-events">
+        <h1>NEWS AND EVENTS</h1>
+        <p>Lorem Ipsum dolor</p>
+
+        <div class="column">
+            <div class="card-news">
+                <img src="miss tourism heritage\image news.png" alt="Avatar" style="width: 100%">
+                <div class="container-news">
+                    <h4><b>SAMPLE NEWS & EVENTS TITLE HERE</b></h4>
+                    <p>Egetr aliquam faucibus sem. Cras in semper ex. Mauris tincidunt purus blandit arcu ullamcorper finibus. Valpolicella Negrar è la storia di uomini e donne dediti alla creazio Nullam dui dolor, sagittis ut ante Maecenas enim justo, facilisis condimentum justo nec, tempor semper tortor. Duis eget orci risus. Pellentesque nec dui sed nulla Lobortis condimentum. Fusce cursus Maecenas enim justo, facilisis condimentum justo nectortor.</p>
+                    <button class="view-det-news">View Details</button>
+                </div>
+            </div>
+
+            <div class="card-news">
+                <img src="miss tourism heritage\image news 02.png" alt="Avatar" style="width: 100%;">
+                <div class="container-news">
+                    <h4><b>SAMPLE NEWS & EVENTS TITLE HERE</b></h4>
+                    <p>Egetr aliquam faucibus sem. Cras in semper ex. Mauris tincidunt purus blandit arcu ullamcorper finibus. Valpolicella Negrar è la storia di uomini e donne dediti alla creazio Nullam dui dolor, sagittis ut ante Maecenas enim justo, facilisis condimentum justo nec, tempor semper tortor. Duis eget orci risus. Pellentesque nec dui sed nulla Lobortis condimentum. Fusce cursus Maecenas enim justo, facilisis condimentum justo nectortor.</p>
+                    <button class="view-det-news">View Details</button>
+                </div>
+            </div>
+
+            <div class="card-news">
+                <img src="miss tourism heritage\image news 03.png" alt="Avatar" style="width: 100%">
+                <div class="container-news">
+                    <h4><b>SAMPLE NEWS & EVENTS TITLE HERE</b></h4>
+                    <p>Egetr aliquam faucibus sem. Cras in semper ex. Mauris tincidunt purus blandit arcu ullamcorper finibus. Valpolicella Negrar è la storia di uomini e donne dediti alla creazio Nullam dui dolor, sagittis ut ante Maecenas enim justo, facilisis condimentum justo nec, tempor semper tortor. Duis eget orci risus. Pellentesque nec dui sed nulla Lobortis condimentum. Fusce cursus Maecenas enim justo, facilisis condimentum justo nectortor.</p>
+                    <button class="view-det-news">View Details</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--footer-->
+    <div class="footer">
+        <div class="column">
+            <div class="f-leftcolumn">
+                <h2>CONTACT DETAILS</h2>
+
+                <div class="column">
+                    <div class="det-logo">
+                        <img src="miss tourism heritage/001-phone-yellow.png" alt="client review" class="c-logo-det">
+                    </div>
+                    <div class="det-text">
+                        <p>8 123-456 Loc. 611 / 612</p>
+                        <p>0912 345 6789</p>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="det-logo">
+                        <img src="miss tourism heritage/002-email-yellow.png" alt="client review" class="c-logo-det">
+                    </div>
+                    <div class="det-text">
+                        <p>info@tourismheritage.com</p>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="det-logo">
+                        <img src="miss tourism heritage/003-wall-clock-yellow.png" alt="client review" class="c-logo-det">
+                    </div>
+                    <div class="det-text">
+                        <p>8:00 AM - 5:00 PM</p>
+                        <p>(Mon - Sat)</p>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="det-logo">
+                        <img src="miss tourism heritage/001-pin-yellow.png" alt="client review" class="c-logo-det">
+                    </div>
+                    <div class="det-text">
+                        <p>Unit 123, Sample address, </p>
+                        <p>for presentation purposes only.</p>
+                    </div>
+                </div>
+
+                <img src="miss tourism heritage/002-facebook-footer.png" alt="client review" class="c-logo">
+                <img src="miss tourism heritage/001-linkedin-footer.png" alt="client review" class="c-logo">
+                <img src="miss tourism heritage/002-paper-plane-footer.png" alt="client review" class="c-logo">
+            </div>
+
+            <div class="f-midcolumn">
+            <h2>QUICK LINKS</h2>
+                <a href="#">HOME</a>
+                <a href="#">ABOUT US</a>
+                <a href="#">OUR DIRECTORS</a>
+                <a href="#">CANDIDATES</a>
+                <a href="#">NEWS AND EVENTS</a>
+                <a href="#">GALLERY</a>
+                <a href="#">CONTACT US</a>
+            </div>
+            
+            <div class="f-rightcolumn">
+                <h2>INQUIRE NOW!</h2>
+
+                <input type="text" value="Name:">
+
+                <input type="text" value="Email:">
+
+                <input type="text" value="Message/Inquiry:" class="input-msg">
+
+                <button class="submit">SEND NOW!</button>
+
+            </div>
+        </div>
+
+        <hr class="footer-line">
+
+        <p class="copyright">
+            © 2021 www.tourismheritage.com All rights reserved. Site Map | Terms of Use | Privacy Policy
+            <img src="miss tourism heritage/itworks_logo.png" alt="client review" class="logo-cp">
+        </p>
+    </div>
+</body>
+</html>
